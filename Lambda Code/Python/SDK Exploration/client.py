04ebdb10893ef0ce1895e02653b7e6f9bf8a8aff
Luqman Olawin <luqmanolawin@Luqmans-MBP.attlocal.net>
2024-03-20 10:31:46 -0400

    added front-end web files

--- Lambda Code/Python/SDK Exploration/client.py
+++ Lambda Code/Python/SDK Exploration/client.py
@@ -13,7 +13,7 @@ import boto3
 
 client = boto3.client('s3')
 
-response = client.list_objects(Bucket='<BUCKET_NAME>')
+response = client.list_objects_v2(Bucket='<BUCKET_NAME>')
 
 for content in response['Contents']:
     obj_dict = client.get_object(Bucket='<BUCKET_NAME>', Key=content['Key'])
